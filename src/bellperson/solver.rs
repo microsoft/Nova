3999a6e91b1dc471975f8411be782b03706daa05
porcuquine <porcuquine@users.noreply.github.com>
2023-05-19 13:55:03 -0700

    Use neptune witness-generation.

--- src/bellperson/solver.rs
+++ src/bellperson/solver.rs
@@ -4,21 +4,10 @@ use crate::traits::Group;
 use ff::Field;
 
 use bellpepper_core::{ConstraintSystem, Index, LinearCombination, SynthesisError, Variable};
-use bellperson::multiexp::DensityTracker;
 
 /// A `ConstraintSystem` which calculates witness values for a concrete instance of an R1CS circuit.
-#[derive(PartialEq)]
-pub struct SatisfyingAssignment<G: Group> {
-  // Density of queries
-  a_aux_density: DensityTracker,
-  b_input_density: DensityTracker,
-  b_aux_density: DensityTracker,
-
-  // Evaluations of A, B, C polynomials
-  a: Vec<G::Scalar>,
-  b: Vec<G::Scalar>,
-  c: Vec<G::Scalar>,
-
+pub struct SatisfyingAssignment<G: Group>
+{
   // Assignments of variables
   pub(crate) input_assignment: Vec<G::Scalar>,
   pub(crate) aux_assignment: Vec<G::Scalar>,
@@ -29,54 +18,27 @@ impl<G: Group> fmt::Debug for SatisfyingAssignment<G> {
   fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
     fmt
       .debug_struct("SatisfyingAssignment")
-      .field("a_aux_density", &self.a_aux_density)
-      .field("b_input_density", &self.b_input_density)
-      .field("b_aux_density", &self.b_aux_density)
-      .field(
-        "a",
-        &self
-          .a
-          .iter()
-          .map(|v| format!("Fr({v:?})"))
-          .collect::<Vec<_>>(),
-      )
-      .field(
-        "b",
-        &self
-          .b
-          .iter()
-          .map(|v| format!("Fr({v:?})"))
-          .collect::<Vec<_>>(),
-      )
-      .field(
-        "c",
-        &self
-          .c
-          .iter()
-          .map(|v| format!("Fr({v:?})"))
-          .collect::<Vec<_>>(),
-      )
       .field("input_assignment", &self.input_assignment)
       .field("aux_assignment", &self.aux_assignment)
       .finish()
   }
 }
 
-impl<G: Group> ConstraintSystem<G::Scalar> for SatisfyingAssignment<G> {
+impl<G: Group> PartialEq for SatisfyingAssignment<G>
+{
+  fn eq(&self, other: &SatisfyingAssignment<G>) -> bool {
+    self.input_assignment == other.input_assignment && self.aux_assignment == other.aux_assignment
+  }
+}
+
+impl<G: Group> ConstraintSystem<G::Scalar> for SatisfyingAssignment<G>
+{
   type Root = Self;
 
   fn new() -> Self {
     let input_assignment = vec![G::Scalar::ONE];
-    let mut d = DensityTracker::new();
-    d.add_element();
 
     Self {
-      a_aux_density: DensityTracker::new(),
-      b_input_density: d,
-      b_aux_density: DensityTracker::new(),
-      a: vec![],
-      b: vec![],
-      c: vec![],
       input_assignment,
       aux_assignment: vec![],
     }
@@ -89,8 +51,6 @@ impl<G: Group> ConstraintSystem<G::Scalar> for SatisfyingAssignment<G> {
     AR: Into<String>,
   {
     self.aux_assignment.push(f()?);
-    self.a_aux_density.add_element();
-    self.b_aux_density.add_element();
 
     Ok(Variable(Index::Aux(self.aux_assignment.len() - 1)))
   }
@@ -102,7 +62,6 @@ impl<G: Group> ConstraintSystem<G::Scalar> for SatisfyingAssignment<G> {
     AR: Into<String>,
   {
     self.input_assignment.push(f()?);
-    self.b_input_density.add_element();
 
     Ok(Variable(Index::Input(self.input_assignment.len() - 1)))
   }
@@ -139,17 +98,53 @@ impl<G: Group> ConstraintSystem<G::Scalar> for SatisfyingAssignment<G> {
   }
 
   fn extend(&mut self, other: &Self) {
-    self.a_aux_density.extend(&other.a_aux_density, false);
-    self.b_input_density.extend(&other.b_input_density, true);
-    self.b_aux_density.extend(&other.b_aux_density, false);
-
-    self.a.extend(&other.a);
-    self.b.extend(&other.b);
-    self.c.extend(&other.c);
-
     self.input_assignment
             // Skip first input, which must have been a temporarily allocated one variable.
             .extend(&other.input_assignment[1..]);
     self.aux_assignment.extend(&other.aux_assignment);
+  }
+
+  fn is_witness_generator(&self) -> bool {
+    true
+  }
+
+  fn extend_inputs(&mut self, new_inputs: &[G::Scalar]) {
+    self.input_assignment.extend(new_inputs);
+  }
+
+  fn extend_aux(&mut self, new_aux: &[G::Scalar]) {
+    self.aux_assignment.extend(new_aux);
+  }
+
+  fn allocate_empty(
+    &mut self,
+    aux_n: usize,
+    inputs_n: usize,
+  ) -> (&mut [G::Scalar], &mut [G::Scalar]) {
+    let allocated_aux = {
+      let i = self.aux_assignment.len();
+      self.aux_assignment.resize(aux_n + i, G::Scalar::ZERO);
+      &mut self.aux_assignment[i..]
+    };
+
+    let allocated_inputs = {
+      let i = self.input_assignment.len();
+      self.input_assignment.resize(inputs_n + i, G::Scalar::ZERO);
+      &mut self.input_assignment[i..]
+    };
+
+    (allocated_aux, allocated_inputs)
+  }
+}
+
+#[allow(dead_code)]
+impl<G: Group> SatisfyingAssignment<G>
+{
+  pub fn scalar_inputs(&self) -> Vec<G::Scalar> {
+    self.input_assignment.clone()
+  }
+
+  pub fn scalar_aux(&self) -> Vec<G::Scalar> {
+    self.aux_assignment.clone()
   }
 }
