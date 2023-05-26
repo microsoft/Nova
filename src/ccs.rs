4f5e78493dbf33bca945cce33275b2dd20edc8d5
oskarth <oskarth@titanproxy.com>
2023-05-26 14:00:23 +0800

    fix(CCS): Add commitment key and CCS is_sat test

--- src/ccs.rs
+++ src/ccs.rs
@@ -2,16 +2,17 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 #![allow(clippy::type_complexity)]
+
 use crate::{
-  constants::{BN_LIMB_WIDTH, BN_N_LIMBS, NUM_HASH_BITS},
+  constants::{BN_LIMB_WIDTH, BN_N_LIMBS, NUM_FE_FOR_RO, NUM_HASH_BITS},
   errors::NovaError,
   gadgets::{
     nonnative::{bignat::nat_to_limbs, util::f_to_nat},
     utils::scalar_as_base,
   },
-  r1cs::R1CSShape,
+  r1cs::{R1CSInstance, R1CSShape, R1CSWitness, R1CS},
   traits::{
-    commitment::CommitmentEngineTrait, AbsorbInROTrait, Group, ROTrait, TranscriptReprTrait,
+    commitment::CommitmentEngineTrait, commitment::CommitmentTrait, AbsorbInROTrait, Group, ROTrait,
   },
   utils::*,
   Commitment, CommitmentKey, CE,
@@ -30,7 +31,7 @@ use sha3::{Digest, Sha3_256};
 // x CCS basic impl
 // x CCS basic is_sat
 // - Clean up old R1CS stuff we don't need
-// - Get rid of hardcoded R1CS
+// x Get rid of hardcoded R1CS
 // - Linearized/Committed CCS
 // - R1CS to CCS
 
@@ -76,8 +77,17 @@ pub struct CCSInstance<G: Group> {
 
 // TODO: Type for other CCS types, eqv to RelaxedR1CS
 
-// TODO: Function to convert R1CS to CCS
-// Put here or in r1cs module
+// TODO: Update variables here? This is currently based on R1CS with M length, not sanity checked
+impl<G: Group> CCS<G> {
+  /// Samples public parameters for the specified number of constraints and variables in an CCS
+  pub fn commitment_key(S: &CCSShape<G>) -> CommitmentKey<G> {
+    let num_cons = S.num_cons;
+    let num_vars = S.num_vars;
+    let total_nz = S.M.iter().fold(0, |acc, m| acc + m.len());
+
+    G::CE::setup(b"ck", max(max(num_cons, num_vars), total_nz))
+  }
+}
 
 // TODO: Util fn to create new CCSShape for R1CS with following values
 // n=n, m=m, N=N, l=l, t=3, q=2, d=2
@@ -383,6 +393,9 @@ impl<G: Group> CCSShape<G> {
   //   Ok((T, comm_T))
   // }
 
+  // TODO: Compute linearized form
+  // Requires MLE
+
   /// Pads the R1CSShape so that the number of variables is a power of two
   /// Renumbers variables to accomodate padded variables
   pub fn pad(&self) -> Self {
@@ -478,5 +491,128 @@ impl<G: Group> CCSInstance<G> {
         X: X.to_owned(),
       })
     }
+  }
+}
+
+#[cfg(test)]
+pub mod test {
+  use super::*;
+  use crate::{
+    r1cs::R1CS,
+    traits::{Group, ROConstantsTrait},
+  };
+  use ::bellperson::{gadgets::num::AllocatedNum, ConstraintSystem, SynthesisError};
+  use ff::{Field, PrimeField};
+  use rand::rngs::OsRng;
+
+  type S = pasta_curves::pallas::Scalar;
+  type G = pasta_curves::pallas::Point;
+
+  #[test]
+  fn test_tiny_ccs() {
+    // 1. Generate valid R1CS Shape
+    // 2. Convert to CCS
+    // 3. Test that it is satisfiable
+
+    let one = S::one();
+    let (num_cons, num_vars, num_io, A, B, C) = {
+      let num_cons = 4;
+      let num_vars = 4;
+      let num_io = 2;
+
+      // Consider a cubic equation: `x^3 + x + 5 = y`, where `x` and `y` are respectively the input and output.
+      // The R1CS for this problem consists of the following constraints:
+      // `I0 * I0 - Z0 = 0`
+      // `Z0 * I0 - Z1 = 0`
+      // `(Z1 + I0) * 1 - Z2 = 0`
+      // `(Z2 + 5) * 1 - I1 = 0`
+
+      // Relaxed R1CS is a set of three sparse matrices (A B C), where there is a row for every
+      // constraint and a column for every entry in z = (vars, u, inputs)
+      // An R1CS instance is satisfiable iff:
+      // Az \circ Bz = u \cdot Cz + E, where z = (vars, 1, inputs)
+      let mut A: Vec<(usize, usize, S)> = Vec::new();
+      let mut B: Vec<(usize, usize, S)> = Vec::new();
+      let mut C: Vec<(usize, usize, S)> = Vec::new();
+
+      // constraint 0 entries in (A,B,C)
+      // `I0 * I0 - Z0 = 0`
+      A.push((0, num_vars + 1, one));
+      B.push((0, num_vars + 1, one));
+      C.push((0, 0, one));
+
+      // constraint 1 entries in (A,B,C)
+      // `Z0 * I0 - Z1 = 0`
+      A.push((1, 0, one));
+      B.push((1, num_vars + 1, one));
+      C.push((1, 1, one));
+
+      // constraint 2 entries in (A,B,C)
+      // `(Z1 + I0) * 1 - Z2 = 0`
+      A.push((2, 1, one));
+      A.push((2, num_vars + 1, one));
+      B.push((2, num_vars, one));
+      C.push((2, 2, one));
+
+      // constraint 3 entries in (A,B,C)
+      // `(Z2 + 5) * 1 - I1 = 0`
+      A.push((3, 2, one));
+      A.push((3, num_vars, one + one + one + one + one));
+      B.push((3, num_vars, one));
+      C.push((3, num_vars + 2, one));
+
+      (num_cons, num_vars, num_io, A, B, C)
+    };
+
+    // create a R1CS shape object
+    let S = {
+      let res = R1CSShape::new(num_cons, num_vars, num_io, &A, &B, &C);
+      assert!(res.is_ok());
+      res.unwrap()
+    };
+
+    // 2. Take R1CS and convert to CCS
+    let S = CCSShape::from_r1cs(S);
+
+    // generate generators and ro constants
+    let _ck = CCS::<G>::commitment_key(&S);
+    let _ro_consts =
+      <<G as Group>::RO as ROTrait<<G as Group>::Base, <G as Group>::Scalar>>::Constants::new();
+
+    // 3. Test that CCS is satisfiable
+    let _rand_inst_witness_generator =
+      |ck: &CommitmentKey<G>, I: &S| -> (S, CCSInstance<G>, CCSWitness<G>) {
+        let i0 = *I;
+
+        // compute a satisfying (vars, X) tuple
+        let (O, vars, X) = {
+          let z0 = i0 * i0; // constraint 0
+          let z1 = i0 * z0; // constraint 1
+          let z2 = z1 + i0; // constraint 2
+          let i1 = z2 + one + one + one + one + one; // constraint 3
+
+          // store the witness and IO for the instance
+          let W = vec![z0, z1, z2, S::zero()];
+          let X = vec![i0, i1];
+          (i1, W, X)
+        };
+
+        let W = {
+          let res = CCSWitness::new(&S, &vars);
+          assert!(res.is_ok());
+          res.unwrap()
+        };
+        let U = {
+          let comm_W = W.commit(ck);
+          let res = CCSInstance::new(&S, &comm_W, &X);
+          assert!(res.is_ok());
+          res.unwrap()
+        };
+
+        // check that generated instance is satisfiable
+        assert!(S.is_sat(ck, &U, &W).is_ok());
+
+        (O, U, W)
+      };
   }
 }
