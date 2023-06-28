99679fb12a854d529e64cbcb421ff129567746a8
CPerezz <c.perezbaro@gmail.com>
2023-06-28 09:41:19 +0200

    change: Move `fix_variables` & util tests to util mod

--- src/ccs/util/mod.rs
+++ src/ccs/util/mod.rs
@@ -27,7 +27,6 @@ use sha3::{Digest, Sha3_256};
 use std::ops::{Add, Mul};
 use std::sync::Arc;
 
-use super::cccs::fix_variables;
 use super::CCSShape;
 pub(crate) mod virtual_poly;
 pub(crate) use virtual_poly::VirtualPolynomial;
@@ -55,4 +54,133 @@ pub fn compute_sum_Mz<G: Group>(
   }
 
   sum_Mz
+}
+
+pub(crate) fn fix_variables<F: PrimeField>(
+  poly: &MultilinearPolynomial<F>,
+  partial_point: &[F],
+) -> MultilinearPolynomial<F> {
+  assert!(
+    partial_point.len() <= poly.get_num_vars(),
+    "invalid size of partial point"
+  );
+  let nv = poly.get_num_vars();
+  let mut poly = poly.Z.to_vec();
+  let dim = partial_point.len();
+  // evaluate single variable of partial point from left to right
+  for (i, point) in partial_point.iter().enumerate() {
+    poly = fix_one_variable_helper(&poly, nv - i, point);
+  }
+
+  MultilinearPolynomial::<F>::new(poly[..(1 << (nv - dim))].to_vec())
+}
+
+fn fix_one_variable_helper<F: PrimeField>(data: &[F], nv: usize, point: &F) -> Vec<F> {
+  let mut res = vec![F::ZERO; 1 << (nv - 1)];
+
+  for i in 0..(1 << (nv - 1)) {
+    res[i] = data[i << 1] + (data[(i << 1) + 1] - data[i << 1]) * point;
+  }
+
+  res
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use pasta_curves::{Ep, Fq};
+  use rand_core::OsRng;
+
+  #[test]
+  fn test_fix_variables() {
+    let A = SparseMatrix::<Ep>::with_coeffs(
+      4,
+      4,
+      vec![
+        (0, 0, Fq::from(2u64)),
+        (0, 1, Fq::from(3u64)),
+        (0, 2, Fq::from(4u64)),
+        (0, 3, Fq::from(4u64)),
+        (1, 0, Fq::from(4u64)),
+        (1, 1, Fq::from(11u64)),
+        (1, 2, Fq::from(14u64)),
+        (1, 3, Fq::from(14u64)),
+        (2, 0, Fq::from(2u64)),
+        (2, 1, Fq::from(8u64)),
+        (2, 2, Fq::from(17u64)),
+        (2, 3, Fq::from(17u64)),
+        (3, 0, Fq::from(420u64)),
+        (3, 1, Fq::from(4u64)),
+        (3, 2, Fq::from(2u64)),
+        (3, 3, Fq::ZERO),
+      ],
+    );
+
+    let A_mle = A.to_mle();
+    let bhc = BooleanHypercube::<Fq>::new(2);
+    for (i, y) in bhc.enumerate() {
+      let A_mle_op = fix_variables(&A_mle, &y);
+
+      // Check that fixing first variables pins down a column
+      // i.e. fixing x to 0 will return the first column
+      //      fixing x to 1 will return the second column etc.
+      let column_i: Vec<Fq> = A
+        .clone()
+        .coeffs()
+        .iter()
+        .copied()
+        .filter_map(|(_, col, coeff)| if col == i { Some(coeff) } else { None })
+        .collect();
+
+      assert_eq!(A_mle_op.Z, column_i);
+
+      // // Now check that fixing last variables pins down a row
+      // // i.e. fixing y to 0 will return the first row
+      // //      fixing y to 1 will return the second row etc.
+      let row_i: Vec<Fq> = A
+        .clone()
+        .coeffs()
+        .iter()
+        .copied()
+        .filter_map(|(row, _, coeff)| if row == i { Some(coeff) } else { None })
+        .collect();
+
+      let mut last_vars_fixed = A_mle.clone();
+      // this is equivalent to Espresso/hyperplonk's 'fix_last_variables' mehthod
+      for bit in y.clone().iter().rev() {
+        last_vars_fixed.bound_poly_var_top(&bit)
+      }
+
+      assert_eq!(last_vars_fixed.Z, row_i);
+    }
+  }
+
+  #[test]
+  fn test_compute_sum_Mz_over_boolean_hypercube() -> () {
+    let z = CCSShape::<Ep>::get_test_z(3);
+    let (ccs, _, _) = CCSShape::<Ep>::gen_test_ccs(&z, &mut OsRng);
+
+    // Generate other artifacts
+    let ck = CCSShape::<Ep>::commitment_key(&ccs);
+    let (_, _, cccs) = ccs.to_cccs_artifacts(&mut OsRng, &ck, &z);
+
+    let z_mle = dense_vec_to_mle(ccs.s_prime, &z);
+
+    // check that evaluating over all the values x over the boolean hypercube, the result of
+    // the next for loop is equal to 0
+    let mut r = Fq::zero();
+    let bch = BooleanHypercube::new(ccs.s);
+    for x in bch.into_iter() {
+      for i in 0..ccs.q {
+        let mut Sj_prod = Fq::one();
+        for j in ccs.S[i].clone() {
+          let sum_Mz: MultilinearPolynomial<Fq> = compute_sum_Mz::<Ep>(&cccs.M_MLE[j], &z_mle);
+          let sum_Mz_x = sum_Mz.evaluate(&x);
+          Sj_prod *= sum_Mz_x;
+        }
+        r += Sj_prod * ccs.c[i];
+      }
+      assert_eq!(r, Fq::ZERO);
+    }
+  }
 }
