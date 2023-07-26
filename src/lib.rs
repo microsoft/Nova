wyattbenno777/SuperNova

cleanup and bug fix

src/lib.rs
@@ -29,10 +29,10 @@ pub mod traits;
 mod ccs;
 #[cfg(feature = "hypernova")]
 mod hypercube;
-#[cfg(feature = "hypernova")]
-mod utils;
 #[cfg(feature = "supernova")]
 mod supernova;
+#[cfg(feature = "hypernova")]
+mod utils;
 
 use crate::bellperson::{
   r1cs::{NovaShape, NovaWitness},

microsoft/Nova

src/lib.rs
@@ -25,6 +25,9 @@ pub mod provider;
 pub mod spartan;
 pub mod traits;
 
+#[cfg(feature = "supernova")]
+mod supernova;
+
 use crate::bellperson::{
   r1cs::{NovaShape, NovaWitness},
   shape_cs::ShapeCS,