b850f3966b569f5d828e510f93c8a3b448088617
oskarth <oskarth@titanproxy.com>
2023-08-06 09:56:39 +0100

    chore: Remove unused imports

--- src/ccs/cccs.rs
+++ src/ccs/cccs.rs
@@ -18,7 +18,6 @@ use crate::{
 use bitvec::vec;
 use core::{cmp::max, marker::PhantomData};
 use ff::{Field, PrimeField};
-use flate2::{write::ZlibEncoder, Compression};
 use itertools::concat;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
