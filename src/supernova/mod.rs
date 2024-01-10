84efea1f833183dbb85f941715fb0311b08ab8f2
François Garillot <4142+huitseeker@users.noreply.github.com>
2024-01-10 17:31:19 -0500

    Fix docs (#249)
    * Refactor 'CircuitShape' references to 'R1CSWithArity'
    
    - Implemented terminology update: all occurrences of `CircuitShape` have been replaced with `R1CSWithArity`
    - Corrected method return descriptions to reflect changes in terminology
    - Amended related notes and comments to maintain consistency and clarity in the codebase.
    
    Leftover from https://github.com/lurk-lab/arecibo/pull/235
    
    * Fix Supernova module documentation and code comments
    
    - Update comments for struct `PublicParams` in `mod.rs` to reflect the correct association with `R1CSWithArity`.
    - Alter codeblock tag in `Readme.md` from `ignore` to `text`.
    - Revise mathematical formula in `Readme.md` for the current iteration of $F$ by including brackets around `0` in the base case inputs.

--- src/supernova/mod.rs
+++ src/supernova/mod.rs
@@ -71,7 +71,7 @@ impl<E: Engine> CircuitDigests<E> {
   }
 }
 
-/// A vector of [CircuitParams] corresponding to a set of [PublicParams]
+/// A vector of [R1CSWithArity] adjoined to a set of [PublicParams]
 #[derive(Clone, Serialize, Deserialize)]
 #[serde(bound = "")]
 pub struct PublicParams<E1, E2, C1, C2>
@@ -288,7 +288,7 @@ where
     (circuit_shapes, aux_params)
   }
 
-  /// Create a [PublicParams] from a vector of raw [CircuitShape] and auxiliary params.
+  /// Create a [PublicParams] from a vector of raw [R1CSWithArity] and auxiliary params.
   pub fn from_parts(circuit_shapes: Vec<R1CSWithArity<E1>>, aux_params: AuxParams<E1, E2>) -> Self {
     let pp = PublicParams {
       circuit_shapes,
@@ -312,7 +312,7 @@ where
     pp
   }
 
-  /// Create a [PublicParams] from a vector of raw [CircuitShape] and auxiliary params.
+  /// Create a [PublicParams] from a vector of raw [R1CSWithArity] and auxiliary params.
   /// We don't check that the `aux_params.digest` is a valid digest for the created params.
   pub fn from_parts_unchecked(
     circuit_shapes: Vec<R1CSWithArity<E1>>,
@@ -335,7 +335,7 @@ where
   }
 
   /// Compute primary and secondary commitment keys sized to handle the largest of the circuits in the provided
-  /// `CircuitShape`.
+  /// `R1CSWithArity`.
   fn compute_primary_ck(
     circuit_params: &[R1CSWithArity<E1>],
     ck_hint1: &CommitmentKeyHint<E1>,
