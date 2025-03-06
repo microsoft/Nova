d18c9d9722ba3a633524f593b272eb383d91a1e6
Srinath Setty <srinath@microsoft.com>
2025-03-06 22:25:03 +0000

    remove unused file

--- src/gadgets/mod.rs
+++ src/gadgets/mod.rs
@@ -1,5 +1,4 @@
 //! This module implements various gadgets necessary for Nova and applications built with Nova.
 pub(crate) mod ecc;
 pub(crate) mod nonnative;
-pub(crate) mod r1cs;
 pub(crate) mod utils;
