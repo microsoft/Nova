f76469869ca047209dac9e846d5445fd472715c5
Srinath Setty <srinath@microsoft.com>
2025-03-13 11:18:16 -0700

    add a benchmark; small improvements to MSM code (#377)
    * add a benchmark; small improvements to MSM code
    
    * fix comment

--- src/provider/secp_secq.rs
+++ src/provider/secp_secq.rs
@@ -1,7 +1,7 @@
 //! This module implements the Nova traits for `secp::Point`, `secp::Scalar`, `secq::Point`, `secq::Scalar`.
 use crate::{
   impl_traits,
-  provider::{msm::cpu_best_msm, traits::DlogGroup},
+  provider::{msm::msm_generic, traits::DlogGroup},
   traits::{Group, PrimeFieldExt, TranscriptReprTrait},
 };
 use digest::{ExtendableOutput, Update};
