b356499dbfe0efba9d2509b31dd0905e4f32232a
Srinath Setty <srinath@microsoft.com>
2025-12-10 17:25:17 -0800

    address clippy

--- src/spartan/polys/multilinear.rs
+++ src/spartan/polys/multilinear.rs
@@ -53,6 +53,11 @@ impl<Scalar: PrimeField> MultilinearPolynomial<Scalar> {
     self.Z.len()
   }
 
+  /// Returns true if the polynomial has no evaluations.
+  pub fn is_empty(&self) -> bool {
+    self.Z.is_empty()
+  }
+
   /// Binds the polynomial's top variable using the given scalar.
   ///
   /// This operation modifies the polynomial in-place.
