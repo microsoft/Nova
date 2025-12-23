ec9524ff8173a99a50eb0903a4fbd2408dae8474
thelitfire <jiaxingzhao@mail.ustc.edu.cn>
2025-12-23 14:06:54 +0800

    Trait for checking key file sanity

--- examples/kzg_keygen_tools.rs
+++ examples/kzg_keygen_tools.rs
@@ -10,7 +10,7 @@ use nova_snark::{
     hyperkzg::{CommitmentEngine, CommitmentKey},
     Bn256EngineKZG,
   },
-  traits::commitment::{CommitmentEngineTrait, SaveTo},
+  traits::commitment::{CommitmentEngineTrait, CommitmentKeyFileTrait},
 };
 use rand_core::OsRng;
 
