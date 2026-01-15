99ce0181e25eda6a104ed28ffd4ff36e57903dea
Srinath Setty <srinath@microsoft.com>
2026-01-15 14:18:18 -0800

    cargo fmt

--- src/spartan/polys/univariate.rs
+++ src/spartan/polys/univariate.rs
@@ -112,34 +112,32 @@ impl<Scalar: PrimeField + CustomSerdeTrait> UniPoly<Scalar> {
   pub fn from_evals(evals: &[Scalar]) -> Self {
     // Lagrange interpolation for evaluations at 0, 1, 2, 3
     assert!(evals.len() >= 3, "at least 3 evaluations required");
-    
+
     if evals.len() == 3 {
       // Quadratic: evaluations at 0, 1, 2
       let two_inv = Scalar::TWO_INV;
       let c = evals[0];
       let a = (evals[0] - evals[1] * Scalar::from(2u64) + evals[2]) * two_inv;
       let b = evals[1] - evals[0] - a;
-      Self { coeffs: vec![c, b, a] }
+      Self {
+        coeffs: vec![c, b, a],
+      }
     } else {
       // Cubic: evaluations at 0, 1, 2, 3
       let two_inv = Scalar::TWO_INV;
       let six_inv = (Scalar::from(6u64)).invert().unwrap();
-      
+
       let c0 = evals[0];
-      let c1 = -Scalar::from(11u64) * six_inv * evals[0]
-             + Scalar::from(3u64) * two_inv * evals[1]
-             - Scalar::from(3u64) * two_inv * evals[2]
-             + Scalar::from(1u64) * six_inv * evals[3];
-      let c2 = evals[0]
-             - Scalar::from(5u64) * two_inv * evals[1]
-             + Scalar::from(2u64) * evals[2]
-             - two_inv * evals[3];
-      let c3 = -six_inv * evals[0]
-             + two_inv * evals[1]
-             - two_inv * evals[2]
-             + six_inv * evals[3];
-      
-      Self { coeffs: vec![c0, c1, c2, c3] }
+      let c1 = -Scalar::from(11u64) * six_inv * evals[0] + Scalar::from(3u64) * two_inv * evals[1]
+        - Scalar::from(3u64) * two_inv * evals[2]
+        + Scalar::from(1u64) * six_inv * evals[3];
+      let c2 = evals[0] - Scalar::from(5u64) * two_inv * evals[1] + Scalar::from(2u64) * evals[2]
+        - two_inv * evals[3];
+      let c3 = -six_inv * evals[0] + two_inv * evals[1] - two_inv * evals[2] + six_inv * evals[3];
+
+      Self {
+        coeffs: vec![c0, c1, c2, c3],
+      }
     }
   }
 
